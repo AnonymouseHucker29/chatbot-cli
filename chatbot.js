e9ec12e88cf64029901627ebed8fdc9e1610d250
AnonymouseHucker29 <brent.baylon29@gmail.com>
2023-07-09 16:11:53 +0800

    minor changes

--- chatbot.js
+++ chatbot.js
@@ -1,8 +1,13 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import { ChatGPTUnofficialProxyAPI } from 'chatgpt';
+import { oraPromise } from 'ora';
+
+dotenv.config();
 
 const blueColor = '\x1b[34m';
 const greenColor = '\x1b[32m';
 const redColor = '\x1b[31m';
+const resetColor = '\x1b[0m';
 
 const prompt = (msg) => {
     process.stdout.write(msg);
@@ -15,35 +20,34 @@ const prompt = (msg) => {
 
 async function chatGPT() {
 
-    const { ChatGPTUnofficialProxyAPI } = await import('chatgpt');
-    const { oraPromise } = await import('ora');
-
     const api = new ChatGPTUnofficialProxyAPI({
         accessToken: process.env.OPENAI_ACCESS_TOKEN,
         apiReverseProxyUrl: process.env.REVERSE_PROXY_URL,
     })
 
     let user = await prompt('Your name: ');
-    process.stdout.write('\033c');
-    console.log(redColor + `Welcome to ChatGPT but diff (low budget version), ${user}! Ask me anything!\nType 'exit' to quit.\n\n`);
+    console.clear();
+    console.log(redColor + `Welcome to ChatGPT but diff (low budget version), ${user}! Ask me anything!\nType 'exit' to quit.\n`);
 
     let conversationId;
     let parentMessageId;
 
     while (true) {
+
         const input = await prompt(blueColor + `${user}: `);
-        console.log('\n');
+        console.log('');
 
         if (input.toLowerCase() === 'exit') {
+            console.log(resetColor);
             return;
         } else {
             const apiResponse = await oraPromise(api.sendMessage(input, {
                 conversationId: conversationId,
                 parentMessageId: parentMessageId,
-                role: 'system',
+                role: 'user',
             }));
 
-            console.log(greenColor + 'BrentGPT: ' + apiResponse.text + '\n\n');
+            console.log(greenColor + 'BrentGPT: ' + apiResponse.text + '\n');
 
             conversationId = apiResponse.conversationId;
             parentMessageId = apiResponse.parentMessageId;
